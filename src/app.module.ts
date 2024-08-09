8eec6a58c40dd06f08f1f8e1221d0cba04b4fdab
Nikita <nikita.pashin.job@gmail.com>
2024-08-10 01:35:21 +0500

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { DialogsModule } from './dialogs/dialogs.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MessagesModule } from './messages/messages.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
+  // controllers: [AppController],
   imports: [DialogsModule, UserModule, AuthModule, MessagesModule],
+  // exports: [AppService],
 })
 export class AppModule {}
